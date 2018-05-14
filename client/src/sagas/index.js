4c49301219335ab105809ef641a83ad2c1ac0d63
bram <david.j.bramwell@googlemail.com>
2018-05-14 18:22:47 +0100

    Request data on app load

--- client/src/sagas/index.js
+++ client/src/sagas/index.js
@@ -1,10 +1,12 @@
 import { all } from 'redux-saga/effects'
-import getBuilds from './request_builds'
+import requestBuilds from './request_builds'
 import startBuild from './request_start_build'
+import getBuilds from './get_builds'
 
 export default function* rootSaga() {
   yield all([
     getBuilds(),
-    startBuild()
+    startBuild(),
+    requestBuilds()
   ])
 }
--- client/src/sagas/start_build.js
+++ client/src/sagas/start_build.js
@@ -1,10 +1,12 @@
-import { call } from 'redux-saga/effects'
+import { call, put } from 'redux-saga/effects'
 import { requestStartBuildAPI } from '../api/request_start_build_api'
-import getBuilds from './get_builds'
+import fetchBuilds from '../actions/fetch_builds'
+import startBuildFailed from '../actions/start_build_failed'
 
 export default function* startBuild() {
   try {
     yield call(requestStartBuildAPI)
+    yield put(fetchBuilds())
   } catch (e) {
     yield put(startBuildFailed(e.message))
   }
